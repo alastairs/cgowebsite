b82617114a611497bd89e111814267a95f7a48e3
Alastair Smith <github@alastairsmith.me.uk>
2012-06-30 18:29:54 +0100

    Add a list of concerts
    Hard-coded list at the moment.

---
+++ src/CGO.Web/Models/Concert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CGO.Web.Models
+{
+    public class Concert
+    {
+        public int Id { get; private set; }
+
+        public string Title { get; private set; }
+        public DateTime DateAndStartTime { get; private set; }
+        public string Location { get; private set; }
+
+        public Concert(int id, string title, DateTime dateAndStartTime, string location)
+        {
+            Id = id;
+            Title = title;
+            DateAndStartTime = dateAndStartTime;
+            Location = location;
+        }
+    }
+}
