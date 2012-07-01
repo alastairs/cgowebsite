41bc8683105be95a0ea4391e0ca5cff1d28d3cd4
Alastair Smith <github@alastairsmith.me.uk>
2012-07-01 21:30:30 +0100

    Begin some tests for Google OAuth integration.
    Also tidy up a bit: move test files into namespaces to mirror the
    production code.

---
+++ src/CGO.Web/Infrastructure/IFormsAuthenticationService.cs
@@ -0,0 +1,18 @@
+using System.Web.Security;
+
+namespace CGO.Web.Infrastructure
+{
+    /// <summary>
+    /// Wrapper interface for <see cref="FormsAuthentication"/>.  
+    /// </summary>
+    public interface IFormsAuthenticationService
+    {
+        /// <summary>
+        /// Creates an authentication ticket for the supplied user name and adds it to the cookies collection of the response, 
+        /// or to the URL if you are using cookieless authentication.
+        /// </summary>
+        /// <param name="username">The name of an authenticated user. This does not have to map to a Windows account. </param>
+        /// <param name="persistent"><c>true</c> to create a persistent cookie (one that is saved across browser sessions); otherwise, <c>false</c>. </param>
+        void SetAuthenticationCookie(string username, bool persistent);
+    }
+}
