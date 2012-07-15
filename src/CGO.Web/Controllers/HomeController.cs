ac3be21403a2037377f47b6a9e8286c9259a3c73
Alastair Smith <github@alastairsmith.me.uk>
2012-07-15 15:34:15 +0100

    Re-do homepage with CGO-related information.

--- src/CGO.Web/Controllers/HomeController.cs
+++ src/CGO.Web/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using CGO.Web.Infrastructure;
+using CGO.Web.Models;
 
 namespace CGO.Web.Controllers
 {
@@ -13,7 +11,18 @@ namespace CGO.Web.Controllers
         {
             ViewBag.Title = "Cambridge Graduate Orchestra";
             ViewBag.GoogleLoginUrl = new GoogleOAuthConfiguration().MakeLoginUri("");
-            return View();
+
+            var model =
+                new HomePageViewModel(
+                    new Concert(3, "Russian Heritage", new DateTime(2012, 12, 01, 20, 0, 0), "West Road Concert Hall"),
+                    new[]
+                        {
+                            new Rehearsal(1, new DateTime(2012, 10, 7, 19, 30, 00), new DateTime(2012, 10, 7, 22, 00, 00), "Hughes Hall"),
+                            new Rehearsal(2, new DateTime(2012, 10, 14, 19, 30, 00), new DateTime(2012, 10, 14, 22, 00, 00), "Hughes Hall"),
+                            new Rehearsal(3, new DateTime(2012, 10, 21, 19, 30, 00), new DateTime(2012, 10, 21, 22, 00, 00), "Hughes Hall")
+                        });
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ src/CGO.Web/Models/HomePageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CGO.Web.Models
+{
+    public class HomePageViewModel
+    {
+        public HomePageViewModel(Concert nextConcert, IEnumerable<Rehearsal> upcomingRehearsals)
+        {
+            NextConcert = nextConcert;
+            UpcomingRehearsals = upcomingRehearsals;
+        }
+
+        public Concert NextConcert { get; private set; }
+        public IEnumerable<Rehearsal> UpcomingRehearsals { get; private set; }
+    }
+}
