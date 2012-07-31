51c692e1009b8d29a6a5b57c273919a455bbee20
Alastair Smith <github@alastairsmith.me.uk>
2012-08-01 00:21:34 +0100

    Add a Concerts API Controller and Delete action.
    This stuff is pretty slick; let's see if I've got the routes defined
    correctly...

---
+++ src/CGO.Web.Tests/Controllers/Api/ConcertsApiControllerFacts.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using CGO.Web.Controllers.Api;
+using CGO.Web.Models;
+
+using NUnit.Framework;
+
+namespace CGO.Web.Tests.Controllers.Api
+{
+    public class ConcertsApiControllerFacts
+    {
+        [TestFixture]
+        public class DeleteShould : RavenTest
+        {
+            private int idToDelete;
+
+            [Test]
+            public void RemoveTheConcertWithTheSpecifiedIdFromTheDatabase()
+            {
+                var controller = new ConcertsApiController(Session);
+
+                controller.Delete(idToDelete);
+
+                Assert.That(Session.Load<Concert>(idToDelete), Is.Null);
+            }
+
+            [SetUp]
+            public void CreateSampleData()
+            {
+                using (var sampleDataSession = Store.OpenSession())
+                {
+                    idToDelete = 1;
+                    sampleDataSession.Store(new Concert(idToDelete, "Foo", DateTime.Now, "Bar"));
+                    sampleDataSession.SaveChanges();
+                }
+            }
+        }
+    }
+}
