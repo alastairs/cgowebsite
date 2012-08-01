ec4780212b9e85e0096c0b3a468a0130466d964b
Alastair Smith <github@alastairsmith.me.uk>
2012-08-02 00:41:23 +0100

    Reimplement the list of concerts with Knockout.
    This provides a richer experience: concerts are automatically added to and
    removed from the list as the user completes their operations.

---
+++ src/CGO.Web/Scripts/ViewModels/ConcertViewModel.js
@@ -0,0 +1,12 @@
+﻿var CGO = CGO || {};
+
+CGO.makeConcertViewModel = function(concert) {
+    var self = {
+        id: concert.Id,
+        title: concert.Title,
+        dateAndStartTime: $.format.date(concert.DateAndStartTime, "d MMM yyyy, HH:mm"),
+        location: concert.Location
+    };
+
+    return self;
+};
