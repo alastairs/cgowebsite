0e78acfd2c85c2d07ecb69bded6c9fe9ae89dfa6
Alastair Smith <github@alastairsmith.me.uk>
2012-08-01 19:08:06 +0100

    GET /concerts
    This returns all the concerts in the database, sorted by date in
    descending order.

--- src/CGO.Web/Controllers/Api/ConcertsController.cs
+++ src/CGO.Web/Controllers/Api/ConcertsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -25,9 +26,10 @@ namespace CGO.Web.Controllers.Api
         }
 
         // GET api/concerts
-        public IEnumerable<string> Get()
+        public IEnumerable<Concert> Get()
         {
-            return new string[] { "value1", "value2" };
+            var concerts = session.Query<Concert>();
+            return concerts.ToList().OrderByDescending(c => c.DateAndStartTime);
         }
 
         // GET api/concerts/5
