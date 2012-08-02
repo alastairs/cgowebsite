e1a5e94f7f7f886cdc3898c19e6046b686d49efc
Alastair Smith <alastair.smith@grantadesign.com>
2012-08-02 14:20:39 +0100

    Fix dates in the Knockout ViewModels.
    Also make the initial GET understand ConcertViewModels rather than
    Concerts directly.

--- src/CGO.Web/Scripts/ViewModels/ConcertViewModel.js
+++ src/CGO.Web/Scripts/ViewModels/ConcertViewModel.js
@@ -4,7 +4,7 @@ CGO.makeConcertViewModel = function(concert) {
     var self = {
         id: concert.Id,
         title: concert.Title,
-        dateAndStartTime: $.format.date(concert.DateAndStartTime, "d MMM yyyy, HH:mm"),
+        dateAndStartTime: $.format.date(new Date(concert.StartTime).toString(), "d MMM yyyy, HH:mm"),
         location: concert.Location
     };
 
