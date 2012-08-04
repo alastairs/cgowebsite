0831ca934f5c9b4de213cf122c959124df3717cc
Alastair Smith <github@alastairsmith.me.uk>
2012-08-04 19:44:00 +0100

    Login should return the Login View.

---
+++ src/CGO.Web.Tests/Controllers/UserControllerFacts.cs
@@ -0,0 +1,25 @@
+﻿using CGO.Web.Controllers;
+
+using NUnit.Framework;
+
+using MvcContrib.TestHelper;
+
+namespace CGO.Web.Tests.Controllers
+{
+    class UserControllerFacts
+    {
+        [TestFixture]
+        public class LoginShould
+        {
+            [Test]
+            public void DisplayTheLoginView()
+            {
+                var controller = new UserController();
+
+                var result = controller.Login();
+
+                result.AssertViewRendered().ForView("Login");
+            }
+        }
+    }
+}
