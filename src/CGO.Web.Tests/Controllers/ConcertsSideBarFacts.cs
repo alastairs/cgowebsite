c68b9770b5451f761ada5ad8809583da4088f58f
Alastair Smith <github@alastairsmith.me.uk>
2012-08-05 14:14:39 +0100

    The first section should be for the current season.
    Just check the title for now, we're going to want to do ordering checks
    and stuff on the links themselves.

--- src/CGO.Web.Tests/Controllers/ConcertsSideBarFacts.cs
+++ src/CGO.Web.Tests/Controllers/ConcertsSideBarFacts.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using System.Web.Mvc;
-using System.Web.Routing;
+using System.ComponentModel;
+using System.Linq;
 
 using CGO.Web.Controllers;
 using CGO.Web.Infrastructure;
+using CGO.Web.Models;
+using CGO.Web.Tests.EqualityComparers;
 
 using NSubstitute;
 
@@ -27,6 +29,76 @@ namespace CGO.Web.Tests.Controllers
             {
                 Assert.That(() => new ConcertsSideBar(Substitute.For<IUrlHelper>(), null), Throws.InstanceOf<ArgumentNullException>());
             }
+        }
+
+        [TestFixture]
+        public class GetSideBarSectionsShould : RavenTest
+        {
+            private SideBarSection currentSeason;
+
+            [Test]
+            public void ReturnTheFirstSectionForTheCurrentSeason()
+            {
+                var sideBar = new ConcertsSideBar(GetMockUrlHelper(), new DocumentSessionFactory(Store));
+
+                var sections = sideBar.GetSideBarSections();
+
+                Assert.That(sections.First().Title, Is.EqualTo(currentSeason.Title));
+            }
+
+            [SetUp]
+            public void CreateExpectedSideBarSections()
+            {
+                currentSeason = new SideBarSection("Current Season", new[]
+                {
+                    new SideBarLink("Concert 1", "/Concerts/Details/1", false),
+                    new SideBarLink("Concert 2", "/Concerts/Details/2", false),
+                    new SideBarLink("Concert 3", "/Concerts/Details/3", false),
+                    new SideBarLink("Concert 4", "/Concerts/Details/4", false)
+                });
+            }
+
+            [SetUp]
+            public void CreateSampleData()
+            {
+                var concerts = new[]
+                {
+                    new Concert(1, "Concert 1", DateTime.Now.AddDays(1d), "Concert Hall"),
+                    new Concert(3, "Concert 3", DateTime.Now.AddDays(3d), "Concert Hall"),
+                    new Concert(2, "Concert 2", DateTime.Now.AddDays(2d), "Concert Hall"),
+                    new Concert(4, "Concert 4", DateTime.Now.AddDays(4d), "Concert Hall")
+                };
+
+                using (var sampleDataSession = Store.OpenSession())
+                {
+                    foreach (var concert in concerts)
+                    {
+                        sampleDataSession.Store(concert);
+                    }
+
+                    sampleDataSession.SaveChanges();
+                }
+            }
+        }
+
+        private static IUrlHelper GetMockUrlHelper()
+        {
+            var mockUrlHelper = Substitute.For<IUrlHelper>();
+            mockUrlHelper.Action(string.Empty, string.Empty, null)
+                         .ReturnsForAnyArgs(callInfo => string.Format("/{1}/{0}/{2}", callInfo.Args()[0],
+                                                                      callInfo.Args()[1], GetIdFromObject(callInfo.Args()[2])));
+
+            return mockUrlHelper;
+        }
+
+        private static object GetIdFromObject(object arg)
+        {
+            var properties = TypeDescriptor.GetProperties(arg);
+
+            return properties.Cast<PropertyDescriptor>()
+                             .Where(property => property.Name == "id")
+                             .Select(property => property.GetValue(arg))
+                             .FirstOrDefault();
         }
     }
 }
--- src/CGO.Web/Controllers/ConcertsSideBar.cs
+++ src/CGO.Web/Controllers/ConcertsSideBar.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using CGO.Web.Infrastructure;
 using CGO.Web.Models;
@@ -22,32 +23,18 @@ namespace CGO.Web.Controllers
 
         public override IEnumerable<SideBarSection> GetSideBarSections()
         {
-            return new[]
+            var currentSeasonSection = new SideBarSection("Current Season");
+            using (var session = documentSessionFactory.CreateSession())
+            {
+                var concerts = session.Query<Concert>().ToList();
+
+                foreach (var concert in concerts)
                 {
-                    new SideBarSection("2012-13 Season", new[]
-                        {
-                            new SideBarLink("Russian Heritage 1 December 2012", Url.Action("Details", "Concerts", new {id = 3}), false),
-                            new SideBarLink("The Witching Hour 8 March 2013", Url.Action("Details", "Concerts", new {id = 0}), false),
-                            new SideBarLink("Beethoven 12 April 2013", Url.Action("Details", "Concerts", new {id = 0}), false),
-                            new SideBarLink("28 June 2013", Url.Action("Details", "Concerts", new {id = 0}), false)
-                        }),
-                    new SideBarSection("Last Season", new[]
-                        {
-                            new SideBarLink("Music Inspired by Fairy Tales", Url.Action("Details", "Concerts", new {id = 0}), false),
-                            new SideBarLink("The Planets", Url.Action("Details", "Concerts", new {id = 0}), false),
-                            new SideBarLink("Music from Germany and Austria", Url.Action("Details", "Concerts", new {id = 1}), false),
-                            new SideBarLink("CGO Around The World", Url.Action("Details", "Concerts", new {id = 2}), false)
-                        }),
-                    new SideBarSection("Older", new[]
-                        {
-                            new SideBarLink("2010-11 Season", Url.Action("Archive", "Concerts", new {year = 2010}), false),
-                            new SideBarLink("2009-10 Season", Url.Action("Archive", "Concerts", new {year = 2009}), false),
-                            new SideBarLink("2008-09 Season", Url.Action("Archive", "Concerts", new {year = 2008}), false),
-                            new SideBarLink("2007-08 Season", Url.Action("Archive", "Concerts", new {year = 2007}), false),
-                            new SideBarLink("2006-07 Season", Url.Action("Archive", "Concerts", new {year = 2006}), false),
-                            new SideBarLink("2005-06 Season", Url.Action("Archive", "Concerts", new {year = 2005}), false)
-                        })
-                };
+                    currentSeasonSection.AddLink(new SideBarLink(concert.Title, Url.Action("Details", "Concerts", new { id = concert.Id }), false));
+                }
+            }
+
+            return new[] { currentSeasonSection };
         }
     }
 }
