a60fe9f4459ee768b86a19448af5fdd1b05458ab
Alastair Smith <github@alastairsmith.me.uk>
2012-08-05 22:51:00 +0100

    Re-do current season section.
    The tests in the previous implementation weren't that great (these are a
    little better, but still icky), and the implementation was a bit off.
    This is improved in this commit by:
    
      - extracting a DateTimeProvider interface and implementation for mocking
        date and time concerns.
      - using a single acceptance test for the entire section, and building
        simpler tests for each requirement for the current season's side bar
        section.

--- src/CGO.Web/Controllers/ConcertsSideBar.cs
+++ src/CGO.Web/Controllers/ConcertsSideBar.cs
@@ -10,33 +10,45 @@ namespace CGO.Web.Controllers
     public class ConcertsSideBar : SideBar
     {
         private readonly IDocumentSessionFactory documentSessionFactory;
+        private readonly IDateTimeProvider dateTimeProvider;
 
-        public ConcertsSideBar(IUrlHelper urlHelper, IDocumentSessionFactory documentSessionFactory) : base(urlHelper)
+        public ConcertsSideBar(IUrlHelper urlHelper, IDocumentSessionFactory documentSessionFactory, IDateTimeProvider dateTimeProvider) : base(urlHelper)
         {
             if (documentSessionFactory == null)
             {
                 throw new ArgumentNullException("documentSessionFactory");
             }
 
+            if (dateTimeProvider == null)
+            {
+                throw new ArgumentNullException("dateTimeProvider");
+            }
+
             this.documentSessionFactory = documentSessionFactory;
+            this.dateTimeProvider = dateTimeProvider;
         }
 
         public override IEnumerable<SideBarSection> GetSideBarSections()
         {
-            var currentSeasonSection = new SideBarSection("Current Season");
-            var lastSeasonSection = new SideBarSection("Last Season");
+            var currentSeason = new SideBarSection("Current Season");
 
-            using (var session = documentSessionFactory.CreateSession())
+            using(var session = documentSessionFactory.CreateSession())
             {
-                var concerts = session.Query<Concert>().OrderBy(c => c.DateAndStartTime).ToList();
-
+                var concerts = session.Query<Concert>().OrderBy(c => c.DateAndStartTime).ToList().Where(ConcertIsInCurrentSeason);
                 foreach (var concert in concerts)
                 {
-                    currentSeasonSection.AddLink(new SideBarLink(concert.Title, Url.Action("Details", "Concerts", new { id = concert.Id }), false));
+                    currentSeason.AddLink(new SideBarLink(concert.Title, Url.Action("Details", "Concerts", new { id = concert.Id }), false));
                 }
             }
 
-            return new[] { currentSeasonSection, lastSeasonSection };
+            return new[] { currentSeason };
+        }
+
+        private bool ConcertIsInCurrentSeason(Concert concert)
+        {
+            var currentYear = dateTimeProvider.Now.Month > 7 ? dateTimeProvider.Now.Year : dateTimeProvider.Now.Year - 1;
+            
+            return concert.DateAndStartTime >= new DateTime(currentYear, 08, 01) && concert.DateAndStartTime <= new DateTime(currentYear + 1, 07, 31);
         }
     }
 }
