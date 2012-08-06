31668157a10e94995e166211a249ce9c2566b3d6
Alastair Smith <github@alastairsmith.me.uk>
2012-08-06 19:14:19 +0100

    Begin Side Bar archive section.
    Return a link for the 2009-10 season.  Implementation is currently
    hardcoded, because I haven't worked out how to implement the solution yet,
    nor think of some good tests...

--- src/CGO.Web.Tests/Controllers/ConcertsSideBarFacts.cs
+++ src/CGO.Web.Tests/Controllers/ConcertsSideBarFacts.cs
@@ -9,6 +9,7 @@ using CGO.Web.Models;
 using CGO.Web.Tests.EqualityComparers;
 
 using NSubstitute;
+using NSubstitute.Core;
 
 using NUnit.Framework;
 
@@ -291,6 +292,73 @@ namespace CGO.Web.Tests.Controllers
                 Assert.That(sections.Skip(1).First(), Is.EqualTo(lastSeasonSection).Using(new SideBarSectionEqualityComparer()));
             }
 
+            [Test]
+            public void ReturnTheThirdSectionForTheArchive()
+            {
+                var concerts = new[] { new Concert(1, "Concert", new DateTime(2010, 06, 25, 20, 00, 00), "Venue") };
+                CreateSampleData(concerts);
+                var sideBar = new ConcertsSideBar(GetMockUrlHelper(), new DocumentSessionFactory(Store), dateTimeProvider);
+
+                var sections = sideBar.GetSideBarSections();
+
+                Assert.That(sections.Skip(2).First().Title, Is.EqualTo("Older"));
+            }
+
+            [Test]
+            public void ReturnALinkForThe2009To2010Season()
+            {
+                var concerts = new[] { new Concert(1, "Concert", new DateTime(2010, 06, 25, 20, 00, 00), "Venue") };
+                CreateSampleData(concerts);
+                var sideBar = new ConcertsSideBar(GetMockUrlHelper(), new DocumentSessionFactory(Store), dateTimeProvider);
+
+                var sections = sideBar.GetSideBarSections();
+
+                var expectedSection = new SideBarSection("Older", new[]
+                {
+                    new SideBarLink("2009-10 Season", "/Concerts/Archive?year=2009", false)
+                });
+                Assert.That(sections.Skip(2).First(), Is.EqualTo(expectedSection).Using(new SideBarSectionEqualityComparer()));
+            }
+
+            [Test]
+            public void PresentTheArchiveAsTheThirdSection()
+            {
+                var currentSeasonConcerts = new[]
+                {
+                    new Concert(4, "Summer Concert", new DateTime(2012, 06, 29, 20, 00, 00), "Venue"),
+                    new Concert(1, "Michaelmas Concert", new DateTime(2011, 11, 18, 20, 00, 00), "Venue"),
+                    new Concert(3, "Spring Concert", new DateTime(2012, 04, 11, 20, 00, 00), "Venue"),
+                    new Concert(2, "Lent Concert", new DateTime(2012, 03, 09, 20, 00, 00), "Venue")
+                };
+                var lastSeasonConcerts = new[]
+                {
+                    new Concert(8, "Summer Concert", new DateTime(2011, 06, 24, 20, 00, 00), "Venue"),
+                    new Concert(5, "Michaelmas Concert", new DateTime(2010, 11, 26, 20, 00, 00), "Venue"),
+                    new Concert(7, "Spring Concert", new DateTime(2011, 04, 15, 20, 00, 00), "Venue"),
+                    new Concert(6, "Lent Concert", new DateTime(2011, 03, 11, 20, 00, 00), "Venue")
+                };
+                var archivedConcerts = new[]
+                {
+                    new Concert(9, "Summer Concert 2010", new DateTime(2010, 06, 25, 20, 00, 00), "Venue"),
+                    new Concert(10, "Summer Concert 2009", new DateTime(2009, 06, 29, 20, 00, 00), "Venue"),
+                    new Concert(11, "Summer Concert 2008", new DateTime(2008, 06, 27, 20, 00, 00), "Venue"),
+                    new Concert(12, "Summer Concert 2007", new DateTime(2007, 06, 14, 20, 00, 00), "Venue")
+                };
+                CreateSampleData(currentSeasonConcerts.Concat(lastSeasonConcerts).Concat(archivedConcerts));
+                var sideBar = new ConcertsSideBar(GetMockUrlHelper(), new DocumentSessionFactory(Store), dateTimeProvider);
+
+                var sections = sideBar.GetSideBarSections();
+
+                var archiveSection = new SideBarSection("Older", new[]
+                {
+                    new SideBarLink("2010-11 Season", "/Concerts/Archive?year=2010", false), 
+                    new SideBarLink("2009-10 Season", "/Concerts/Archive?year=2009", false), 
+                    new SideBarLink("2008-09 Season", "/Concerts/Archive?year=2008", false), 
+                    new SideBarLink("2007-08 Season", "/Concerts/Archive?year=2007", false)
+                });
+                Assert.That(sections.Skip(2).First(), Is.EqualTo(archiveSection).Using(new SideBarSectionEqualityComparer()));
+            }
+
             [SetUp]
             public void FixDateTimeNow()
             {
@@ -316,19 +384,30 @@ namespace CGO.Web.Tests.Controllers
         {
             var mockUrlHelper = Substitute.For<IUrlHelper>();
             mockUrlHelper.Action(string.Empty, string.Empty, null)
-                         .ReturnsForAnyArgs(callInfo => string.Format("/{1}/{0}/{2}", callInfo.Args()[0],
-                                                                      callInfo.Args()[1], GetIdFromObject(callInfo.Args()[2])));
+                         .ReturnsForAnyArgs(callInfo => GetUrl(callInfo.Args()[0] as string, callInfo.Args()[1] as string, callInfo.Args()[2]));
 
             return mockUrlHelper;
         }
 
-        private static object GetIdFromObject(object arg)
+        private static string GetUrl(string actionName, string controllerName, object urlArgument)
         {
-            var properties = TypeDescriptor.GetProperties(arg);
+            var idArgument = GetItemFromObject(urlArgument, "id");
+            if (idArgument != null)
+            {
+                return string.Format("/{1}/{0}/{2}", actionName,
+                                 controllerName, idArgument);
+            }
+
+            return string.Format("/{1}/{0}?year={2}", actionName, controllerName, GetItemFromObject(urlArgument, "year"));
+        }
+
+        private static object GetItemFromObject(object urlArgument, string argName)
+        {
+            var properties = TypeDescriptor.GetProperties(urlArgument);
 
             return properties.Cast<PropertyDescriptor>()
-                             .Where(property => property.Name == "id")
-                             .Select(property => property.GetValue(arg))
+                             .Where(property => property.Name == argName)
+                             .Select(property => property.GetValue(urlArgument))
                              .FirstOrDefault();
         }
     }
