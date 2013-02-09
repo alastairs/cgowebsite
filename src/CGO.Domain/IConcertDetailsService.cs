263997f6ffd8b715859161adf12cfe9a7806b67d
Alastair Smith <github@alastairsmith.me.uk>
2013-02-09 15:52:05 +0000

    Add two domain services for Concerts.
    The Concert details service allows clients to request a single concert by ID.
    
    The Concerts' Season service allows clients to request all concerts in the current season, all concerts in the previous season, and all concerts in an arbitrary season (identified by the start year of the season).

---
+++ src/CGO.Domain/IConcertDetailsService.cs
@@ -0,0 +1,15 @@
+﻿namespace CGO.Domain
+{
+    /// <summary>
+    /// A service for retrieving details about one or more Concerts
+    /// </summary>
+    public interface IConcertDetailsService
+    {
+        /// <summary>
+        /// Return a single Concert with the given identity
+        /// </summary>
+        /// <param name="concertId">The identifier for the requested Concert.</param>
+        /// <returns></returns>
+        Concert GetConcert(int concertId);
+    }
+}
