bf59a0d321fcdc93e93a2f74c7ef1e0ebdb072ca
Alastair Smith <github@alastairsmith.me.uk>
2013-02-09 16:04:29 +0000

    Skeleton implementation of concert details service
    This implementation does nothing at all at the moment, just throws a NotImplementedException.

---
+++ src/CGO.DataAccess.Raven/RavenConcertDetailsService.cs
@@ -0,0 +1,12 @@
+﻿using CGO.Domain;
+
+namespace CGO.DataAccess.Raven
+{
+    public class RavenConcertDetailsService : IConcertDetailsService
+    {
+        public Concert GetConcert(int concertId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
