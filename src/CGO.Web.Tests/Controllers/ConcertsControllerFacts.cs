1367d2d6828b37f1d4e11ff02cc1d14323747cfd
Alastair Smith <github@alastairsmith.me.uk>
2013-02-09 20:30:29 +0000

    Use new service in Archive action.
    The Archive tests no longer need to be RavenTests, and one test has been deleted because it's now covered by the Raven integration tests written previously.

--- src/CGO.Web.Tests/Controllers/ConcertsControllerFacts.cs
+++ src/CGO.Web.Tests/Controllers/ConcertsControllerFacts.cs
@@ -389,14 +389,12 @@ namespace CGO.Web.Tests.Controllers
         }
 
         [TestFixture]
-        public class ArchiveShould : RavenTest
+        public class ArchiveShould
         {
-            private IReadOnlyCollection<Concert> concerts2009;
-
             [Test]
             public void ReturnArchiveView()
             {
-                var controller = new ConcertsController(Session, Substitute.For<IConcertDetailsService>(),
+                var controller = new ConcertsController(Substitute.For<IDocumentSession>(), Substitute.For<IConcertDetailsService>(),
                                                         Substitute.For<IConcertsSeasonService>());
 
                 var result = controller.Archive(2009);
@@ -407,7 +405,7 @@ namespace CGO.Web.Tests.Controllers
             [Test]
             public void ReturnArchiveViewWithConcerts()
             {
-                var controller = new ConcertsController(Session, Substitute.For<IConcertDetailsService>(),
+                var controller = new ConcertsController(Substitute.For<IDocumentSession>(), Substitute.For<IConcertDetailsService>(),
                                                         Substitute.For<IConcertsSeasonService>());
 
                 var result = controller.Archive(2009);
@@ -416,63 +414,30 @@ namespace CGO.Web.Tests.Controllers
             }
 
             [Test]
-            public void ReturnArchiveWithTheConcertsFromTheRequestedSeason()
+            public void ReturnArchiveWithTheConcertsFromTheRequestedSeasonFromTheService()
             {
-                var controller = new ConcertsController(Session, Substitute.For<IConcertDetailsService>(),
-                                                        Substitute.For<IConcertsSeasonService>());
-                IReadOnlyCollection<Concert> expectedData = concerts2009.ToArray();
+                var concertsSeasonService = Substitute.For<IConcertsSeasonService>();
+                var concert = new Concert(1, "Test Concert", DateTime.MinValue, "Venue");
+                IReadOnlyCollection<Concert> expectedConcerts = new[] { concert };
+                concertsSeasonService.GetConcertsInSeason(2009).Returns(expectedConcerts);
+                var controller = new ConcertsController(Substitute.For<IDocumentSession>(), Substitute.For<IConcertDetailsService>(),
+                                                        concertsSeasonService);
 
                 var result = controller.Archive(2009) as ViewResult;
                 var viewModel = result.Model as IReadOnlyCollection<Concert>;
 
-                Assert.That(viewModel, Is.EqualTo(expectedData).Using(new ConcertEqualityComparer()));
-            }
-
-            [Test]
-            public void ReturnOnlyPublishedConcertsFromTheRequestedSeason()
-            {
-                var controller = new ConcertsController(Session, Substitute.For<IConcertDetailsService>(),
-                                                        Substitute.For<IConcertsSeasonService>());
-                var unpublishedConcert = new Concert(1, "Unpublished 2009 Concert", new DateTime(2009, 11, 14), "West Road Concert Hall");
-                Session.Store(unpublishedConcert);
-                Session.SaveChanges();
-
-                var result = controller.Archive(2009) as ViewResult;
-                var viewModel = result.Model as IReadOnlyCollection<Concert>;
-
-                Assert.That(viewModel, Is.Not.Contains(unpublishedConcert).Using(new ConcertEqualityComparer()));
+                Assert.That(viewModel, Is.EqualTo(expectedConcerts).Using(new ConcertEqualityComparer()));
             }
 
             [Test]
             public void SetTheConcertSeasonPropertyInTheViewBag()
             {
-                var controller = new ConcertsController(Session, Substitute.For<IConcertDetailsService>(),
+                var controller = new ConcertsController(Substitute.For<IDocumentSession>(), Substitute.For<IConcertDetailsService>(),
                                                         Substitute.For<IConcertsSeasonService>());
 
                 var result = controller.Archive(2009) as ViewResult;
 
                 Assert.That(result.ViewBag.ConcertSeason, Is.EqualTo("2009-2010"));
-            }
-
-            [SetUp]
-            public void CreateTestData()
-            {
-                concerts2009 = new List<Concert>
-                {
-                    new Concert(2, "Michaelmas", new DateTime(2009, 11, 13), "West Road Concert Hall"), 
-                    new Concert(3, "Lent", new DateTime(2010, 02, 26), "West Road Concert Hall"), 
-                    new Concert(4, "Spring", new DateTime(2010, 04, 2), "West Road Concert Hall"), 
-                    new Concert(5, "Summer", new DateTime(2010, 06, 25), "West Road Concert Hall")
-                };
-
-                var concerts2009List = concerts2009.ToList();
-                concerts2009List.ForEach(c => c.Publish());
-
-                using (var sampleDataSession = Store.OpenSession())
-                {
-                    concerts2009List.ForEach(sampleDataSession.Store);          
-                    sampleDataSession.SaveChanges();
-                }
             }
         }
 
