8b338bb03f8399584043b1557ca8645ce2035c43
Alastair Smith <github@alastairsmith.me.uk>
2013-02-09 16:42:54 +0000

    Add unit test project for Raven concert services.
    Add a first unit test as well, to ensure that GetConcert() calls Session.Load().

---
+++ src/CGO.DataAccess.Raven.Tests/RavenConcertDetailsServiceFacts.cs
@@ -0,0 +1,26 @@
+﻿using CGO.Domain;
+using NSubstitute;
+using NUnit.Framework;
+using Raven.Client;
+
+namespace CGO.DataAccess.Raven.Tests
+{
+    public class RavenConcertDetailsServiceFacts
+    {
+        [TestFixture]
+        public class GetConcertShould
+        {
+            [Test]
+            public void CallSessionLoadWithTheProvidedIdentity()
+            {
+                var ravenSession = Substitute.For<IDocumentSession>();
+                var concertDetailsService = new RavenConcertDetailsService(ravenSession);
+                const int concertId = 3;
+
+                concertDetailsService.GetConcert(concertId);
+
+                ravenSession.Received().Load<Concert>(concertId);
+            }
+        }
+    }
+}
