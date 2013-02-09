b5dfc17b899ed1534795af5d915e99435147ebf0
Alastair Smith <github@alastairsmith.me.uk>
2013-02-09 16:28:30 +0000

    Add IntegrationTests project for Raven

---
+++ src/CGO.DataAccess.Raven.IntegrationTests/RavenConcertDetailsServiceFacts.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CGO.DataAccess.Raven.IntegrationTests
+{
+    public class RavenConcertDetailsServiceFacts
+    {
+        [TestFixture]
+        public class GetConcertShould : RavenTest
+        {
+            [Test]
+            public void ReturnTheRequestedConcert()
+            {
+                
+            }
+        }
+    }
+}
---
+++ src/CGO.DataAccess.Raven.IntegrationTests/RavenTest.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Embedded;
+
+namespace CGO.DataAccess.Raven.IntegrationTests
+{
+    public abstract class RavenTest
+    {
+        protected IDocumentStore Store { get; private set; }
+        protected IDocumentSession Session { get; private set; }
+
+        [SetUp]
+        public void ConfigureRavenDb()
+        {
+            Store = new EmbeddableDocumentStore { RunInMemory = true };
+            Store.Initialize();
+        }
+
+        [SetUp]
+        public void OpenSession()
+        {
+            Session = Store.OpenSession();
+        }
+
+        [TearDown]
+        public void CloseSession()
+        {
+            Session.Dispose();
+        }
+
+        [TearDown]
+        public void DestroyRavenDbStore()
+        {
+            Store.Dispose();
+        }
+    }
+}
