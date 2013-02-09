5fad89d027c2337d7af3d479a363cf04a7d7b60f
Alastair Smith <github@alastairsmith.me.uk>
2013-02-09 18:35:01 +0000

    Add GetFutureConcerts() operation.
    The first test ensures that only concerts in the future are returned.

--- src/CGO.DataAccess.Raven/RavenConcertDetailsService.cs
+++ src/CGO.DataAccess.Raven/RavenConcertDetailsService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CGO.Domain;
 using Raven.Client;
 
@@ -7,20 +9,34 @@ namespace CGO.DataAccess.Raven
     public class RavenConcertDetailsService : IConcertDetailsService
     {
         private readonly IDocumentSession ravenSession;
+        private readonly IDateTimeProvider dateTimeProvider;
 
-        public RavenConcertDetailsService(IDocumentSession ravenSession)
+        public RavenConcertDetailsService(IDocumentSession ravenSession, IDateTimeProvider dateTimeProvider)
         {
             if (ravenSession == null)
             {
                 throw new ArgumentNullException("ravenSession");
             }
 
+            if (dateTimeProvider == null)
+            {
+                throw new ArgumentNullException("dateTimeProvider");
+            }
+
             this.ravenSession = ravenSession;
+            this.dateTimeProvider = dateTimeProvider;
         }
 
         public Concert GetConcert(int concertId)
         {
             return ravenSession.Load<Concert>(concertId);
+        }
+
+        public IReadOnlyCollection<Concert> GetFutureConcerts()
+        {
+            return ravenSession.Query<Concert>()
+                               .Where(c => c.DateAndStartTime > dateTimeProvider.Now)
+                               .ToList();
         }
     }
 }
--- src/CGO.Domain/IConcertDetailsService.cs
+++ src/CGO.Domain/IConcertDetailsService.cs
@@ -1,4 +1,6 @@
-﻿namespace CGO.Domain
+﻿using System.Collections.Generic;
+
+namespace CGO.Domain
 {
     /// <summary>
     /// A service for retrieving details about one or more Concerts
@@ -11,5 +13,11 @@
         /// <param name="concertId">The identifier for the requested Concert.</param>
         /// <returns></returns>
         Concert GetConcert(int concertId);
+
+        /// <summary>
+        /// Return all published concerts that occur in the future, ordered by ascending date.
+        /// </summary>
+        /// <returns>A collection of concerts in the published state.</returns>
+        IReadOnlyCollection<Concert> GetFutureConcerts();
     }
 }
