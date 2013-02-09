8e54b0928ab68a06c9d67ff17ba0757ec2a93711
Alastair Smith <github@alastairsmith.me.uk>
2013-02-09 16:42:54 +0000

    Add unit test project for Raven concert services.
    Add a first unit test as well, to ensure that GetConcert() calls Session.Load().

---
+++ src/CGO.DataAccess.Raven.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CGO.DataAccess.Raven.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CGO.DataAccess.Raven.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a35526f9-005b-4f66-9f0f-a4d94804f86f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/CGO.DataAccess.Raven/RavenConcertDetailsService.cs
+++ src/CGO.DataAccess.Raven/RavenConcertDetailsService.cs
@@ -1,12 +1,27 @@
-﻿using CGO.Domain;
+﻿using System;
+using CGO.Domain;
+using Raven.Client;
 
 namespace CGO.DataAccess.Raven
 {
     public class RavenConcertDetailsService : IConcertDetailsService
     {
+        private readonly IDocumentSession ravenSession;
+
+        public RavenConcertDetailsService(IDocumentSession ravenSession)
+        {
+            if (ravenSession == null)
+            {
+                throw new ArgumentNullException("ravenSession");
+            }
+
+            this.ravenSession = ravenSession;
+        }
+
         public Concert GetConcert(int concertId)
         {
-            throw new System.NotImplementedException();
+            ravenSession.Load<Concert>(concertId);
+            return null;
         }
     }
 }
