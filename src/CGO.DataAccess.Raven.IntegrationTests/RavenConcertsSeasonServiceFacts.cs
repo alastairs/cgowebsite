2117aba19d15abd280972411dc7582bffa383eca
Alastair Smith <github@alastairsmith.me.uk>
2013-02-09 19:49:29 +0000

    Return concerts from the requested season.
    GetConcertsInSeason() should return only concerts from the requested season.

---
+++ src/CGO.DataAccess.Raven.IntegrationTests/RavenConcertsSeasonServiceFacts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CGO.Domain;
+using NUnit.Framework;
+
+namespace CGO.DataAccess.Raven.IntegrationTests
+{
+    class RavenConcertsSeasonServiceFacts
+    {
+        [TestFixture]
+        public class GetConcertsInSeasonShould : RavenTest
+        {
+            private List<Concert> concerts2009;
+
+            [Test]
+            public void ReturnTheConcertsInTheRequestedSeason()
+            {
+                var concertsSeasonService = new RavenConcertsSeasonService(Session);
+                IReadOnlyCollection<Concert> expected2009Concerts = concerts2009.ToArray();
+
+                var actual2009Concerts = concertsSeasonService.GetConcertsInSeason(2009);
+
+                Assert.That(actual2009Concerts.Select(c => c.Id), Is.EqualTo(expected2009Concerts.Select(c => c.Id)));
+            }
+            
+            [SetUp]
+            public void CreateTestData()
+            {
+                concerts2009 = new List<Concert>
+                {
+                    new Concert(2, "Michaelmas", new DateTime(2009, 11, 13), "West Road Concert Hall"), 
+                    new Concert(3, "Lent", new DateTime(2010, 02, 26), "West Road Concert Hall"), 
+                    new Concert(4, "Spring", new DateTime(2010, 04, 2), "West Road Concert Hall"), 
+                    new Concert(5, "Summer", new DateTime(2010, 06, 25), "West Road Concert Hall")
+                };
+
+                concerts2009.ForEach(c => c.Publish());
+
+                using (var sampleDataSession = Store.OpenSession())
+                {
+                    concerts2009.ForEach(sampleDataSession.Store);
+                    sampleDataSession.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ src/CGO.DataAccess.Raven/RavenConcertsSeasonService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CGO.Domain;
+using Raven.Client;
+
+namespace CGO.DataAccess.Raven
+{
+    public class RavenConcertsSeasonService : IConcertsSeasonService
+    {
+        private readonly IDocumentSession ravenSession;
+
+        public RavenConcertsSeasonService(IDocumentSession ravenSession)
+        {
+            if (ravenSession == null)
+            {
+                throw new ArgumentNullException("ravenSession");
+            }
+
+            this.ravenSession = ravenSession;
+        }
+
+        public IReadOnlyCollection<Concert> GetConcertsInCurrentSeason()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<Concert> GetConcertsInPreviousSeason()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<Concert> GetConcertsInSeason(int seasonStartYear)
+        {
+            return ravenSession.Query<Concert>().ToList();
+        }
+    }
+}
