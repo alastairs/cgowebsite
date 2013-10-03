4c306bcd785826b94a20bba228062f0bd7d62c57
Alastair Smith <alastair.smith@grantadesign.com>
2013-10-03 16:57:36 +0100

    Add new projects for Simple.Data data access layer
    Two class libraries, one for production code, one for tests. Include the
    first test and its implementation.

---
+++ src/CGO.DataAccess.SimpleData.Tests/SimpleDataConcertDetailsRepositoryFacts.cs
@@ -0,0 +1,18 @@
+﻿using CGO.Domain;
+using NUnit.Framework;
+
+namespace CGO.DataAccess.SimpleData.Tests
+{
+    public class SimpleDataConcertDetailsRepositoryFacts
+    {
+        [TestFixture]
+        public class ItShould
+        {
+            [Test]
+            public void ImplementIConcertDetailsService()
+            {
+                Assert.That(new SimpleDataConcertDetailsRepository(), Is.AssignableTo<IConcertDetailsService>());
+            }
+        }
+    }
+}
---
+++ src/CGO.DataAccess.SimpleData/SimpleDataConcertDetailsRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CGO.Domain;
+
+namespace CGO.DataAccess.SimpleData
+{
+    public class SimpleDataConcertDetailsRepository : IConcertDetailsService
+    {
+        public Concert GetConcert(int concertId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IReadOnlyCollection<Concert> GetFutureConcerts()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveConcert(Concert concert)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
