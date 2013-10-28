2d142cb2ca2b70b9265aea7e0878b7bdad457c77
Alastair Smith <github@alastairsmith.me.uk>
2013-10-28 20:57:26 +0000

    Add configuration details for RavenHQ.
    Compile the existing RavenModule only in debug environments, and create a
    new ReleaseRavenModule for production. This connects to RavenHQ via a
    connection string defined in Azure.

---
+++ src/CGO.Web/NinjectModules/DebugRavenModule.cs
@@ -0,0 +1,34 @@
+﻿#if DEBUG
+using Ninject;
+using Ninject.Modules;
+using Ninject.Web.Common;
+
+using Raven.Client;
+using Raven.Client.Embedded;
+
+namespace CGO.Web.NinjectModules
+{
+    public class DebugRavenModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind<IDocumentStore>().ToMethod(_ => InitialiseDocumentStore()).InSingletonScope();
+            Kernel.Bind<IDocumentSession>().ToMethod(_ => Kernel.Get<IDocumentStore>().OpenSession()).InRequestScope();
+        }
+
+        private static IDocumentStore InitialiseDocumentStore()
+        {
+            var documentStore = new EmbeddableDocumentStore
+            {
+                DataDirectory = "CGO.raven",
+                Configuration = { Port = 28645 }
+            };
+
+            documentStore.Initialize();
+            documentStore.InitializeProfiling();
+
+            return documentStore;
+        }
+    }
+}
+#endif
