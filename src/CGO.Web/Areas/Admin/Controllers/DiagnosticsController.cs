609f3d452d5be75a7f7e2c481620063860f95f62
Alastair Smith <alastair.smith@grantadesign.com>
2013-10-29 12:21:09 +0000

    Add an Admin page for diagnostics information.
    Currently, just output all the connection strings the site knows about.

---
+++ src/CGO.Web/Areas/Admin/Controllers/DiagnosticsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace CGO.Web.Areas.Admin.Controllers
+{
+    public class DiagnosticsController : Controller
+    {
+        //
+        // GET: /Admin/Diagnostics/
+
+        public ActionResult Index()
+        {
+            return View(ConfigurationManager.ConnectionStrings);
+        }
+
+    }
+}
