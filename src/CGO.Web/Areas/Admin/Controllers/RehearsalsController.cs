d177d1e998fb44c55d86b3618894cacc523ca3f7
Alastair Smith <github@alastairsmith.me.uk>
2013-10-31 11:58:51 +0000

    Add the beginnings of a Rehearsal edit form.
    Also include the controller action to render the form, the view model for
    the form, and a link to the new form in the menu bar (via another dropdown
    list).

---
+++ src/CGO.Web/Areas/Admin/Controllers/RehearsalsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using CGO.Web.Areas.Admin.Models;
+
+namespace CGO.Web.Areas.Admin.Controllers
+{
+    public class RehearsalsController : AdminController
+    {
+        //
+        // GET: /Admin/Rehearsals/Create
+        public ActionResult Create()
+        {
+            return View(new RehearsalViewModel());
+        }
+
+        //
+        // POST: /Admin/Rehearsals/Create
+        [HttpPost]
+        public ActionResult Create(RehearsalViewModel viewModel)
+        {
+            return View();
+        }
+	}
+}
---
+++ src/CGO.Web/Areas/Admin/Models/RehearsalViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CGO.Web.Areas.Admin.Models
+{
+    public class RehearsalViewModel
+    {
+        public int Id { get; set; }
+        public string Venue { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsSectionalRehearsal { get; set; }
+        public IEnumerable<string> RequiredSections { get; set; }
+
+        public IEnumerable<string> Sections
+        {
+            get
+            {
+                return new[] {"Strings", "Woodwind", "Brass", "Percussion", "Tutti"};
+            }
+        }
+    }
+}
